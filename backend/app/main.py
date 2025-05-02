3dd3f4b7560eb4919f3290cfffd7d2536c107862
Davi Dantas Soares <davi.dantas@intellissis.com.br>
2025-05-02 17:55:58 -0300

    feat : adds mongo connection, mongo image and hot-reload

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+import os
+from motor.motor_asyncio import AsyncIOMotorClient
 
 app = FastAPI()
+MONGODB_URL = os.environ.get("MONGODB_URL")
+
+client = AsyncIOMotorClient(MONGODB_URL)
+db = client.get_default_database()
+
+print(db)
 
 @app.get("/")
 async def root():
-    return {"message": "Hello, World!"}
+    return {"message": "Hello, slap na flap na tchex!"}
