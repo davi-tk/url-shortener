a449039b3d4d4ddca334392fdcd71acb385eb895
Davi Dantas Soares <davi.dantas@intellissis.com.br>
2025-05-09 13:07:49 -0300

    base layout for upgrading on

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,6 @@
 import { Link } from "@heroui/link";
+import { Input } from "@heroui/input";
+import { Button } from "@heroui/button";
 import { Snippet } from "@heroui/snippet";
 import { Code } from "@heroui/code";
 import { button as buttonStyles } from "@heroui/theme";
@@ -9,48 +11,24 @@ import { GithubIcon } from "@/components/icons";
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Make&nbsp;</span>
-        <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
-        <br />
-        <span className={title()}>
-          websites regardless of your design experience.
-        </span>
-        <div className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
-        </div>
-      </div>
+    <>
+      <main className="grid grid-rows-3 gap-16 justify-items-center items-center">
+        <h3>SIMPLIFY YOUR LINKS</h3>
+        <h2>Transform Long Urls Into Tiny Links Instantly!</h2>
+         <Input
+          label="Website"
+          labelPlacement="outside"
+          placeholder="heroui.com"
+          startContent={
+            <div className="pointer-events-none flex items-center">
+              <span className="text-default-400 text-small">https://</span>
+            </div>
+          }
+          type="url"
+        />
 
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href={siteConfig.links.docs}
-        >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </div>
-
-      <div className="mt-8">
-        <Snippet hideCopyButton hideSymbol variant="bordered">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-        </Snippet>
-      </div>
-    </section>
+        <Button size="md">Generate Link - <code>its free</code></Button>
+      </main>
+    </>
   );
 }
