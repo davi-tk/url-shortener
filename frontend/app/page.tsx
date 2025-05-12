6edf886f1932c77a0d4809b3d28e3fc06862944c
Davi Dantas Soares <davi.dantas@intellissis.com.br>
2025-05-12 13:24:52 -0300

    reorganizes imports

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,13 @@
-import { Link } from "@heroui/link";
 import { Input } from "@heroui/input";
 import { Button } from "@heroui/button";
-import { Snippet } from "@heroui/snippet";
-import { Code } from "@heroui/code";
-import { button as buttonStyles } from "@heroui/theme";
+import { Form } from "@heroui/form"
+import { useState } from "react";
 
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
 
 export default function Home() {
+
+  const [url, setUrl] = useState("")
+
   return (
     <>
     <div className="grid grid-cols-6 h-min">
@@ -17,7 +15,7 @@ export default function Home() {
           <h3 className="text-3xl">SIMPLIFY YOUR LINKS</h3>
           <h2 className="text-5xl">Transform Long Urls Into Tiny Links Instantly!</h2>
 
-            <form className="w-full grid grid-rows-2 gap-2 row-span-2">
+            <Form className="w-full grid grid-rows-2 gap-2 row-span-2">
               <Input
               className="w-full"
               label="Website"
@@ -31,9 +29,13 @@ export default function Home() {
               type="url"
               radius="none"
             />
-            <Button className="w-full" size="md" radius="none">Generate Link - <code className="italic">its free</code></Button>
+            <Button 
+              className="w-full" 
+              size="md" 
+              radius="none"
+              type="submit">Generate Link - <span className="italic">its free</span></Button>
           
-            </form>
+            </Form>
             
         </main>
       </div>
