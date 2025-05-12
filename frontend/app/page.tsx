ca7968ef538cff405d452c6b61dd1a99c4fd8a2b
Davi Dantas Soares <davi.dantas@intellissis.com.br>
2025-05-12 19:27:23 -0300

    adds OnSubmit logic

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,17 @@
+"use client"
 import { Input } from "@heroui/input";
 import { Button } from "@heroui/button";
 import { Form } from "@heroui/form"
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 
 export default function Home() {
+  const [url, setUrl] = useState<string>("")
 
-  const [url, setUrl] = useState("")
+  const onSubmit = (e:FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(url)
+  }
 
   return (
     <>
@@ -15,28 +20,26 @@ export default function Home() {
           <h3 className="text-3xl">SIMPLIFY YOUR LINKS</h3>
           <h2 className="text-5xl">Transform Long Urls Into Tiny Links Instantly!</h2>
 
-            <Form className="w-full grid grid-rows-2 gap-2 row-span-2">
+            <Form
+              className="w-full grid grid-rows-2 gap-2 row-span-2"
+              onSubmit={onSubmit}>
               <Input
               className="w-full"
-              label="Website"
+              label="Url"
               labelPlacement="outside"
-              placeholder="heroui.com"
-              startContent={
-                <div className="pointer-events-none flex items-center">
-                  <span className="text-default-400 text-small">https://</span>
-                </div>
-              }
+              placeholder="your-url.com"
               type="url"
               radius="none"
-            />
-            <Button 
-              className="w-full" 
-              size="md" 
-              radius="none"
-              type="submit">Generate Link - <span className="italic">its free</span></Button>
+              value={url}
+              onValueChange={setUrl}
+              />
+              <Button 
+                className="w-full" 
+                size="md" 
+                radius="none"
+                type="submit">Generate Link - <span className="italic">its free</span></Button>
           
             </Form>
-            
         </main>
       </div>
     </>
